fda51ea9da5bd4464f0f9d98af81c207eeea29b0
Iavor-Valentin Iftime <valiiftime@google.com>
2024-12-11 02:37:21 -0800

    [BACKPORT] Check sound Uri permission when creating a notification channel
     Verify that the calling app has the right permissions to read/grant the sound Uri
     for a channel it creates.
     Previously, Uri permissions would only be checked by NotificationRecord when a notification was posted.
    
    Flag: EXEMPT security fix
    
    Test: atest PreferencesHelperTest
    Test: atest NotificationManagerServiceTest
    Bug: 337775777
    
    (cherry picked from commit 94fc92cf20b897f016c3a9718edf40818de4a11b)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:665e1aee2dbad19d436a08d753b4d5cf733abeb5)
    Merged-In: If5a6c80e3bd665a9e67655a230e43d704422082b
    Change-Id: If5a6c80e3bd665a9e67655a230e43d704422082b

--- services/core/java/com/android/server/notification/NotificationRecord.java
+++ services/core/java/com/android/server/notification/NotificationRecord.java
@@ -1291,7 +1291,9 @@ public final class NotificationRecord {
      * {@link SecurityException} depending on target SDK of enqueuing app.
      */
     private void visitGrantableUri(Uri uri, boolean userOverriddenUri) {
-        if (uri == null || !ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())) return;
+        if (mGrantableUris != null && mGrantableUris.contains(uri)) {
+            return; // already verified this URI
+        }
 
         // We can't grant Uri permissions from system
         final int sourceUid = getSbn().getUid();
@@ -1299,11 +1301,15 @@ public final class NotificationRecord {
 
         final long ident = Binder.clearCallingIdentity();
         try {
-            // This will throw SecurityException if caller can't grant
-            mUgmInternal.checkGrantUriPermission(sourceUid, null,
-                    ContentProvider.getUriWithoutUserId(uri),
-                    Intent.FLAG_GRANT_READ_URI_PERMISSION,
-                    ContentProvider.getUserIdFromUri(uri, UserHandle.getUserId(sourceUid)));
+            if (uri != null && ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())) {
+                Binder.withCleanCallingIdentity(() -> {
+                    mUgmInternal.checkGrantUriPermission(sourceUid, null,
+                            ContentProvider.getUriWithoutUserId(uri),
+                            Intent.FLAG_GRANT_READ_URI_PERMISSION,
+                            ContentProvider.getUserIdFromUri(uri,
+                            UserHandle.getUserId(sourceUid)));
+                });
+            }
 
             if (mGrantableUris == null) {
                 mGrantableUris = new ArraySet<>();
@@ -1317,8 +1323,6 @@ public final class NotificationRecord {
                     Log.w(TAG, "Ignoring " + uri + " from " + sourceUid + ": " + e.getMessage());
                 }
             }
-        } finally {
-            Binder.restoreCallingIdentity(ident);
         }
     }
 
